4de3ab742a6be6b58e468dfc1a241d4a982923d9
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-06-22 14:03:53 -0300

    criando a class students

---
+++ school.ts
@@ -0,0 +1,13 @@
+class Studant {
+  matricula: string;
+  nome: string;
+  notasProva: number[];
+  notasTrabalho: number[];
+
+  constructor(
+    matricula: string,
+    nome: string,
+    notasProva: number[],
+    notasTrabalho: number[],
+  ) {}
+}
